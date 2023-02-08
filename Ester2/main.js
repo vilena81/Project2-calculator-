26f476bbb0c017b47e593750e049eb1df5d36b81
EsterAbrahamyan <esteranun@gmail.com>
2023-02-08 10:26:04 +0400

    news2

--- Ester2/main.js
+++ Ester2/main.js
@@ -15,7 +15,7 @@ let buttons = [
   {value: '1', id: 'one'},
   {value: '2', id: 'two'},
   {value: '3', id: 'three'},
-  {value: '+', id: 'add'},
+  {value: '+', id: 'add operator'},
   {value: '00', id: 'double-zero'},
   {value: '0', id: 'zero'},
   {value: '=', id: 'equals'}
@@ -28,20 +28,57 @@ let Calculate = (number) => {
     result.value += number;
 };
 
-let Result = () => {
-    let elements = result.value.split('+');
+//let Result = () => {
+    /*let elements = result.value.split('+');
     let num1 = parseInt(elements[0]);
     let num2 = parseInt(elements[1]);
     let sum = num1 + num2;
     console.log(num1,num2,sum)
     //result.value = eval(result.value);
-    result.value=sum
+    result.value=sum*/
+//}
+/*let result = document.getElementById("result");
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {*/
+
+
+
+
+let Result = () => {
+    console.log(result.value)
+   // result.value = eval(result.value);
+   
+//let operator = document.getElementById("operator").value;
+console.log(result.value)
+  if (result.value.includes('+')) {
+    let elements = result.value.split('+');
+    let num1 = parseFloat(elements[0]);
+    let num2 = parseFloat(elements[1]);
+    let sum = num1 + num2;
+    result.value = sum;
+  } else if(result.value.includes('-')) {
+    let elements = result.value.split('-');
+    let num1 = parseFloat(elements[0]);
+    let num2 = parseFloat(elements[1]);
+    let difference = num1 - num2;
+    result.value = difference;
+  } else if (result.value.includes('*')) {
+    let elements = result.value.split('*');
+    let num1 = parseFloat(elements[0]);
+    let num2 = parseFloat(elements[1]);
+    let product = num1 * num2;
+    result.value = product;
+  } else if (result.value.includes('/')) {
+    let elements = result.value.split('/');
+    let num1 = parseFloat(elements[0]);
+    let num2 = parseFloat(elements[1]);
+    let quotient = num1 / num2;
+    result.value = quotient;
+  }
+
 };
     
-  
-
-
-
 function clr() {
     result.value = "";
 }
